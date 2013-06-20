78d88c5c3ddf4cdd46ec3983dd1a6ae2b1035a2f
Christian O'Reilly <christian.oreilly@umontreal.ca>
2013-06-20 17:08:18 -0400

    Adding/correcting some scipts useful for the developement.

---
+++ copyToSitepackages.bat
@@ -0,0 +1,2 @@
+cd C:\Users\revestech\Dropbox\code\Spyndle
+xcopy  /E /Y spyndle\*.py* C:\Python27\Lib\site-packages\spyndle-0.1.1a.dev1-py2.7.egg\spyndle\
--- packaging.bat
+++ packaging.bat
@@ -1,4 +1,4 @@
-pushd C:\Users\revestech\Dropbox\Spyndle
+pushd C:\Users\revestech\Dropbox\code\Spyndle
 
 # register the package with PyPI, creates a source and
 # an egg distribution. Do not upload it to PyPI because of 
