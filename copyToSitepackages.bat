39b6c049b4110cb1b8e4f9c9acd8bf92a083481a
unknown <oreichri@dreamstudent7.crhsc.local>
2013-07-30 19:48:50 -0400

    Adding instructions to the copyToSitepackages.bat script such that .svg and .png files are copied to the site-package folder (for the EEG subpackage).

--- copyToSitepackages.bat
+++ copyToSitepackages.bat
@@ -1,2 +1,3 @@
-cd C:\Users\revestech\Dropbox\code\Spyndle
 xcopy  /E /Y spyndle\*.py* C:\Python27\Lib\site-packages\spyndle-0.1.1a.dev1-py2.7.egg\spyndle\
+xcopy  /E /Y spyndle\*.svg* C:\Python27\Lib\site-packages\spyndle-0.1.1a.dev1-py2.7.egg\spyndle\
+xcopy  /E /Y spyndle\*.png* C:\Python27\Lib\site-packages\spyndle-0.1.1a.dev1-py2.7.egg\spyndle\
