b3202178eeeb8b53d3065fea23c149a249123a1c
unknown <oreichri@dreamstudent7.crhsc.local>
2013-07-30 19:53:05 -0400

    Many code improvement and adding of some functions to plot arrow diagram (plotArrowsBidirect, plotArrosDifferential, plotArrowDiagram). Also, the HeadDrawing object can now contain a colorbar.

--- spyndle/EEG/mapping.py
+++ spyndle/EEG/mapping.py
@@ -1,24 +1,54 @@
 # -*- coding: utf-8 -*-
+
+
 """
-Created on Fri Jul 27 16:21:15 2012
+    Copyright (C) 2012-2013  Christian O'Reilly
 
-@author: REVESTECH
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+
+    For personnal, educationnal, and research purpose, this software is 
+    provided under the GNU GPL (V.3) license: you can redistribute it and/or
+    modify it under the terms of the version 3 of the GNU General Public 
+    License as published by the Free Software Foundation.
+          
+    To use this software in commercial application, please contact the author. 
+    If used for research purpose, the reference [1] or references [2-3] should 
+    be cited in the derived publication to refere the reader to the description 
+    of the methodology.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+ Author: Christian O'Reilly (christian.oreilly@umontreal.ca)
+ Date  : July 27, 2012
+
 """
-import sys, os
-sys.path.append('\\\\HERMES\\DNL-net\\Members\\Christian\\Python\\src\\')
-
-
-from scipy.interpolate import interp2d, bisplrep, bisplev, NearestNDInterpolator, griddata
-from scipy import array, reshape, concatenate, dot, arange, meshgrid, transpose
-from scipy import arctan2, pi, cos, sin, zeros, where, isnan
-from EEG import colorElectrodes
-
+import  os
 import numpy as np
-
 import re
-
-
 import math
+
+import matplotlib.image as mpimg
+import matplotlib.gridspec as gridspec
+import matplotlib            
+
+from scipy.interpolate import griddata
+from scipy import array, reshape, concatenate, dot, arange, meshgrid, nan, unique
+from scipy import arctan2, pi, cos, sin, zeros, where, isnan
+
+from spyndle.EEG import electrodesSVG
+
+
 
 def hsv2rgb(h, s, v):
     h = float(h)
@@ -55,7 +85,7 @@ def getTransformedCoord(cx, cy, transform):
 
 
 def getHeadCircle():
-    cx, cy, rx, ry, transform = colorElectrodes.getHeadCircleProp()
+    cx, cy, rx, ry, transform = electrodesSVG.getHeadCircleProp()
 
     r = arange(0, 2.0*pi+0.1, 0.1)
     xl = float(rx)*cos(r) + float(cx)
@@ -79,13 +109,10 @@ def getHeadCircle():
 
 
 def showColorMap(listeElectrodes, zVal):
-
+    import pylab    
 
     zz = getZZ(listeElectrodes, zVal)
 
-    import matplotlib.image as mpimg
-    import pylab
-    
     circle = getHeadCircle()
     pylab.plot(circle[:, 0], circle[:, 1], 'k')    
             
@@ -101,7 +128,7 @@ def showColorMap(listeElectrodes, zVal):
 
 def getZZ(listeElectrodes, zVal):
 
-    cx, cy, transform = colorElectrodes.getElectrodeCoordinates(listeElectrodes)
+    cx, cy, transform = electrodesSVG.getElectrodeCoordinates(listeElectrodes)
     
     x = []
     y = []
@@ -133,15 +160,15 @@ def getZZ(listeElectrodes, zVal):
 
 
 def computeColorMapPval(listeElectrodes, zVal, pVal, minZZ=-0.02, maxZZ=0.02):
+
+    import pylab        
+    
     zz = getZZ(listeElectrodes, zVal)
     pp = getZZ(listeElectrodes, pVal)
     
     zz[where(isnan(zz))] = 0.0
     pp[where(isnan(pp))] = 0.0
 
-    import matplotlib.image as mpimg
-    import pylab
-    
     circle = getHeadCircle()
     pylab.plot(circle[:, 0], circle[:, 1], 'k')    
             
@@ -181,6 +208,9 @@ def computeColorMapPval(listeElectrodes, zVal, pVal, minZZ=-0.02, maxZZ=0.02):
 
 
 def computeColorMap(listeElectrodes, zVal, minZZ=0.00, maxZZ=0.02):
+        
+    import pylab    
+        
     zz = getZZ(listeElectrodes, zVal)
     
     zz[where(isnan(zz))] = 0.0
@@ -188,9 +218,6 @@ def computeColorMap(listeElectrodes, zVal, minZZ=0.00, maxZZ=0.02):
     #s[where(isnan(zz))] = 0.0
     #print s
 
-    import matplotlib.image as mpimg
-    import pylab
-    
     circle = getHeadCircle()
     pylab.plot(circle[:, 0], circle[:, 1], 'k')    
             
@@ -282,16 +309,16 @@ class headDrawing:
     def __init__(self):
         from matplotlib import patches
         
-        self.listeElectrodes = ['Fp1', 'Fp2', 'F3', 'F4', 'F7', 'F8', 'C3', 'C4', 'P3', 
-                            'P4', 'O1', 'O2', 'T3', 'T4', 'T5', 'T6', 'Fz', 'Cz', 'Pz']    
+        self.__electrodeList = ['Fp1', 'Fp2', 'F3', 'F4', 'F7', 'F8', 'C3', 'C4', 'P3', 
+                                'P4', 'O1', 'O2', 'T3', 'T4', 'T5', 'T6', 'Fz', 'Cz', 'Pz']    
   
         self.cx = {}
         self.cy = {}
         self.rx = {} 
         self.ry = {}
         self.electrodePatch = {}
-        for elect in self.listeElectrodes :
-            self.cx[elect], self.cy[elect], self.rx[elect], self.ry[elect] = transformCircle(colorElectrodes.getElectCircleProp(elect))  
+        for elect in self.__electrodeList :
+            self.cx[elect], self.cy[elect], self.rx[elect], self.ry[elect] = transformCircle(electrodesSVG.getElectCircleProp(elect))  
             self.electrodePatch[elect] = patches.Ellipse((self.cx[elect], self.cy[elect]), 2.0*self.rx[elect], 
                                                                               2.0*self.ry[elect], angle=0.0, fc="white")
     
@@ -299,6 +326,10 @@ class headDrawing:
         
         self.ColorMap = None
         self.s        = None
+    
+    
+    def setElectrodeList(self, electrodeList):
+        self.__electrodeList = electrodeList
     
     
     def setElectrodeColor(self, elect, color):
@@ -370,23 +401,24 @@ class headDrawing:
                     self.ColorMap[i1, i2, 1] = float(g)/255.0
                     self.ColorMap[i1, i2, 2] = float(b)/255.0
      
-
-
     
-    def plot(self, save=False, filename="fig.svg"):
+    def plot(self, save=False, filename="fig.svg", colorbarDict=None):
             
         if save:
-            import matplotlib
             matplotlib.use('Agg')
-            
-        import matplotlib.image as mpimg
-            
-        import pylab
+        import pylab    
     
     
-        cx, cy, rx, ry = transformCircle(colorElectrodes.getHeadCircleProp())
+        cx, cy, rx, ry = transformCircle(electrodesSVG.getHeadCircleProp())
         
-        pylab.figure(figsize=(10, 10))         
+        fig = pylab.figure(figsize=(10, 10.5))   
+        
+        if not colorbarDict is None:    
+            gs = gridspec.GridSpec(1, 2, width_ratios=[10, 0.5]) 
+            ax = pylab.subplot(gs[0])            
+        else:
+            ax = pylab.gca()        
+        
         #thismanager = pylab.get_current_fig_manager()
         #thismanager.window.wm_geometry("500x500+0+0")     
     
@@ -400,9 +432,8 @@ class headDrawing:
         img=mpimg.imread(os.path.dirname(os.path.realpath(__file__))  + "\\eeg_electrodes_10-20_small_clear.png")  
         pylab.imshow(img, extent=[cx-rx-65, cx+rx+65, cy+ry+27, cy-ry-80.5])  
     
-        ax=pylab.gca()
     
-        for elect in self.listeElectrodes : 
+        for elect in self.__electrodeList : 
             ax.add_patch(self.electrodePatch[elect] )
     
 
@@ -418,9 +449,115 @@ class headDrawing:
             ax.add_patch(arrow)
                 
         pylab.axis('off')
-        pylab.show()
+        
+        if not colorbarDict is None:      
+            ax = pylab.subplot(gs[1])
+            my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap',colorbarDict, 256)
+            data = np.outer(np.arange(101),np.ones(5)) 
+            pylab.imshow(data, cmap=my_cmap, origin="lower")
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().tick_right()
+            ticks = np.arange(0, 101, 25)
+            ax.set_yticks(ticks)
+            ax.set_yticklabels(np.round(ticks/100.0*(colorbarDict["max"]-colorbarDict["min"])+colorbarDict["min"], 4))
+            gs.tight_layout(fig)
+
+
 
         if save:
             pylab.savefig(filename,dpi=100, transparent=False, bbox_inches='tight', pad_inches=0)
+        else:
+            pylab.show()
 
 
+
+
+
+
+
+
+
+def plotArrowsBidirect(meanVal, pairs, minDelay=None, maxDelay=None, 
+                                      filename=None, addColorbar=True):
+
+    listVal = []
+    for ref, test in pairs :
+        try:        
+            listVal.append(meanVal[ref][test])
+        except KeyError:
+            listVal.append(nan)
+    
+    listVal = array(listVal)        
+    
+    plotArrowDiagram(listVal, pairs, 
+                     minDelay, maxDelay, filename, addColorbar)    
+
+
+
+def plotArrowsDifferential(meanVal, pairs, minDelay=None, maxDelay=None, 
+                          filename=None, addColorbar=True):
+    
+    listVal = []
+    for ref, test in pairs :
+        try:        
+            listVal.extend([meanVal[ref][test] - meanVal[test][ref]])
+        except KeyError:
+            listVal.append(nan)
+    
+    
+    
+    listVal = array(listVal)
+    
+    IND       = where(listVal > 0.0)[0]
+    listVal   = listVal[IND]
+    pairs     = [pairs[i] for i in IND]
+    
+    plotArrowDiagram(listVal, pairs, 
+                     minDelay, maxDelay, filename, addColorbar)
+    
+    
+    
+def plotArrowDiagram(listVal, pairs, minDelay=None, maxDelay=None, 
+                          filename=None, addColorbar=True):    
+    
+    if isinstance(listVal, list):
+        listVal = array(listVal)
+    
+    if minDelay is None :
+        minDelay = min(listVal[np.logical_not(np.isnan(listVal))])
+    if maxDelay is None :    
+        maxDelay = max(listVal[np.logical_not(np.isnan(listVal))])
+    
+    normDelay = (listVal - minDelay)/(maxDelay - minDelay)
+    normDelay = np.maximum(normDelay, 0.0)
+    normDelay = np.minimum(normDelay, 1.0)
+
+    drawing = headDrawing()
+    refs = unique([pair[0] for pair in pairs])
+    drawing.setElectrodeList(refs)
+
+    for pair, delay in zip(pairs, normDelay) :
+        if not isnan(delay):
+            r  = min(1.0, delay*2.0)
+            gb = max(0.0, (delay*2.0 - 1.0)*200/255)                     
+            drawing.addArrow(pair[0], pair[1], (r, gb, gb), 1.0)        
+    
+    if addColorbar:
+        cdict = {'red': ((0.0, 0.0, 0.0),
+                         (0.5, 1.0, 1.0),
+                         (1.0, 1.0, 1.0)),
+                 'green': ((0.0, 0.0, 0.0),
+                           (0.5, 0.0, 0.0),
+                           (1.0, 200./255., 200./255.)),
+                 'blue': ((0.0, 0.0, 0.0),
+                          (0.5, 0.0, 0.0),
+                          (1.0, 200./255., 200./255.)),
+                 'min':minDelay,
+                 'max':maxDelay}    
+    else:
+        cdict = None
+    
+    if filename is None:
+        drawing.plot(False, colorbarDict=cdict)
+    else:
+        drawing.plot(True, filename, colorbarDict=cdict)
