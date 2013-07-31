b67797e8b14d98b0f70ad146f87d2108079fc355
unknown <oreichri@dreamstudent7.crhsc.local>
2013-07-30 20:02:22 -0400

    Updating the root __ini__.py script to take into account that we added the miscellaneous subpackage.

--- spyndle/__init__.py
+++ spyndle/__init__.py
@@ -2,7 +2,8 @@
 __version__ = "0.1.1a.dev1"
 
 #from propagation.XCST import computeXCST
-from filters import Filter, channelType
-from sleepCycles import cycleDefinitions, computeDreamCycles
-from STransform import computeMST, computeST
-from fastST import computeFastST, computeFastST_real
+from miscellaneous.filters import Filter, channelType
+from miscellaneous.sleepCycles import cycleDefinitions, computeDreamCycles
+from miscellaneous.STransform import computeMST, computeST
+from miscellaneous.fastST import computeFastST, computeFastST_real
+from miscellaneous.line import Line, Point
