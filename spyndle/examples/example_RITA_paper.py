0b2a7a363646fc515632166ec1ded14513bb9e2e
unknown <oreichri@OREILLYC.crhsc.local>
2013-08-01 01:49:44 -0400

    Updating the RITA example for the final version of the initial submission of the paper.

--- spyndle/examples/example_RITA_paper.py
+++ spyndle/examples/example_RITA_paper.py
@@ -1,14 +1,16 @@
 from spyndle.io import EDFReader
 from spyndle.detector import SpindleDectectorRMS
-from spyndle.propagation import computeXCST, computeSPF_allFiles,computeAveragePropagation
-from spyndle.EEG import getAdjacentsPairs_10_20, plotArrowsBidirect, plotColorMap
+from spyndle.propagation import computeXCST, computeSPF_allFiles, \
+    computeAveragePropagation, applyRejectionC3
+from spyndle.EEG import getAdjacentsPairs_10_20, plotArrowsBidirect, \
+    plotColorMap
 from spyndle.EEG.system10_20 import getEEGChannels
 
 from urllib import  urlretrieve,  ContentTooShortError
 from scipy import unique, array
 import pandas 
-from datetime import datetime                                                  ##########################################
-A = datetime.now()                                                             ##########################################
+from datetime import datetime                                                  
+A = datetime.now()                                                             
 
 fileName        = "RITA_example.BDF"
 detectionStages = ["Sleep stage 2"]
@@ -24,8 +26,8 @@ except ContentTooShortError:
      print "The retreived file is shorter than expected. The download as "\
            "probably been interrupted"
      exit
-B = datetime.now()                                                             ##########################################
-print B-A                                                                      ##########################################
+B = datetime.now()                                                             
+print B-A                                                                      
 
 print "Reading the .bdf file..."
 readerEDF    = EDFReader(fileName)
@@ -36,49 +38,50 @@ detector = SpindleDectectorRMS(readerEDF)
 detector.setDetectionStages(detectionStages)
 detector.detectSpindles(listChannels)
 detector.saveSpindle(readerEDF, eventName)
-C = datetime.now()                                                             ##########################################
-print C-B                                                                      ##########################################
+C = datetime.now()                                                             
+print C-B                                                                      
 
 
 print "Computing synchrone comparisons..."
 computeXCST(EDFReader, fileName, "", eventName, listChannels, 
                  delta =0.5, beforePad=0.5, afterPad=0.5, 
                  offset=0.0, eventProperties = [],
-                 resFilesPrefix="spindleDelays")
-D = datetime.now()                                                             ##########################################
-print D-C                                                                      ##########################################
+                 resFilesPrefix="spindleDelays", similIndexType="euclidean")
+D = datetime.now()                                                             
+print D-C                                                                      
 
 print "Computing asynchrone comparisons..."
 computeXCST(EDFReader, fileName, "", eventName, listChannels, 
                  delta =0.5, beforePad=0.5, afterPad=0.5, 
                  offset=5.0, eventProperties = [],
-                 resFilesPrefix="spindleDelays_offset")
+                 resFilesPrefix="spindleDelays_offset", similIndexType="euclidean")
 
-E = datetime.now()                                                             ##########################################
-print E-D                                                                      ##########################################
+E = datetime.now()                                                             
+print E-D                                                                      
 
 print "Computing SPFs..."
 computeSPF_allFiles("", offsetFilePattern="spindleDelays_offset_*.BDF_*.txt",
                     alpha = 10.0, verbose = False)
-F = datetime.now()                                                             ##########################################
-print F-E                                                                      ##########################################
+F = datetime.now()                                                             
+print F-E                                                                      
 
 
 print "Computing averages..."    
 computeAveragePropagation("", aggeragationlevels = ["ref", "test"],
-                          observationVariables = ["delay", "duration", "similarity"], 
-                          pattern="correctedData_*.csv",  verbose=True)
-G = datetime.now()                                                             ##########################################
-print G-F                                                                      ##########################################
+                  observationVariables = ["delay", "duration", "similarity"], 
+                  pattern="correctedData_*.csv",  verbose=False)
+G = datetime.now()                                                             
+print G-F                                                                      
 
 propData = pandas.read_csv("meanData.csv", sep=";")
 
 print "Plotting the results in result_example_a.png..."
-medData = propData.groupby("ref").median()
+medData    = propData.groupby("ref").median()
 electrodes = array(medData.index, dtype=str)
 plotColorMap(electrodes, medData.duration_mean, "result_example_a.png")
                             
 print "Plotting the results in result_example_b.png..."
+propData = applyRejectionC3(propData)
 adjPairs = getAdjacentsPairs_10_20(unique(array(propData.ref)))
-medData = propData.groupby(["ref", "test"]).median()["delay_mean"]
+medData  = propData.groupby(["ref", "test"]).median()["delay_mean"]
 plotArrowsBidirect(medData, adjPairs, filename="result_example_b.png")
