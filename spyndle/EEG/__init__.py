44837bca1ac77ba86c453114aaa67957fae082fb
unknown <oreichri@dreamstudent7.crhsc.local>
2013-09-10 17:42:30 -0400

    Adding getEEGChannels to the EEG init file.

--- spyndle/EEG/__init__.py
+++ spyndle/EEG/__init__.py
@@ -1,5 +1,8 @@
 # __init__.py
 
-from system10_20 import get10_20AdjacentElectrodes, getAdjacentsPairs_10_20, getActiveElectrode
+from system10_20 import get10_20AdjacentElectrodes, getAdjacentsPairs_10_20, \
+    getActiveElectrode, getEEGChannels
 from electrodesSVG import getElectrodeCoordinates
-from mapping import plotArrowsBidirect, HeadDrawing, plotColorMap, plotArrowsDifferential
+from mapping import plotArrowsBidirect, HeadDrawing, plotColorMap, \
+    plotArrowsDifferential
+    
