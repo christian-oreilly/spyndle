659bd5402801fc2c4728069a06bbf750653f78f1
unknown <oreichri@dreamstudent7.crhsc.local>
2013-09-17 14:24:31 -0400

    Adding some imports to allow importing modules (e.g., from spyndle import io).

--- spyndle/__init__.py
+++ spyndle/__init__.py
@@ -1,6 +1,7 @@
 # __init__.py
 __version__ = "0.2.1.dev1"
 
+
 #from propagation.XCST import computeXCST
 from miscellaneous.filters import Filter, channelType
 from miscellaneous.sleepCycles import cycleDefinitions, computeDreamCycles
@@ -8,3 +9,5 @@ from miscellaneous.STransform import computeMST, computeST
 from miscellaneous.fastST import computeFastST, computeFastST_real
 from miscellaneous.line import Line, Point
 from miscellaneous.utils import setUnbufferedPrint, Unbuffered, terminate_process, diff2
+
+import io, DevuystDB, detector, miscellaneous, propagation, EEG
