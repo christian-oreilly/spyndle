4b276f2aea142c74b93862e3707e070b0249a183
unknown <oreichri@dreamstudent7.crhsc.local>
2013-12-10 14:16:52 -0500

    Modifications to the line.py file to provide a more pythonic (less QTic) interface.

--- spyndle/miscellaneous/line.py
+++ spyndle/miscellaneous/line.py
@@ -33,84 +33,186 @@
 
 """
 
+from PySide import QtCore, QtGui
 from numpy import sqrt
+
+
+class Polygon(QtGui.QPolygon):
+    def __init__(self, points):
+        
+        QtGui.QPolygon.__init__(self)        
+        for point in points:
+            if isinstance(point, Point):
+                point = QtCore.QPoint(point.x, point.y)
+            elif not isinstance(point, QtCore.QPoint):
+                raise TypeError("The points argument contain an item which is equal to " \
+                                + str(point) + " and of type " + str(type(point)))
+            
+            self.append(point)
+            
+        
 
 class Point(object):
     def __init__(self, x=0, y=0):
-        self.setPoint(x, y)
+        if isinstance(x, QtCore.QPoint):
+            self.setPoint(x.x(), x.y())
+        else:
+            self.setPoint(x, y)
     
     def setPoint(self, x, y):
-        self.__x = x
-        self.__y = y
+        self._x = x
+        self._y = y
 
-    def setx(self, x):
-        self.__x = x
+
+    def QPoint(self):
+        return QtCore.QPoint(self.x, self.y)
+
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
         
-    def sety(self, y):
-        self.__y = y
-
-    def x(self) : return self.__x
-    def y(self) : return self.__y
-
+    
 
     def __sub__(self, p2):
-        return Point(self.x()-p2.x(), self.y()-p2.y())
+        if isinstance(p2, QtCore.QPoint):
+            p2 = Point(p2)      
+        elif not isinstance(p2, Point):
+            raise TypeError("The p2 argument is equal to " \
+                            + str(p2) + " and of type " + str(type(p2)))
+        return Point(self.x-p2.x, self.y-p2.y)
 
     def __add__(self, p2):
-        return Point(self.x()+p2.x(), self.y()+p2.y())
+        if isinstance(p2, QtCore.QPoint):
+            p2 = Point(p2)      
+        elif not isinstance(p2, Point):
+            raise TypeError("The p2 argument is equal to " \
+                            + str(p2) + " and of type " + str(type(p2)))
+        return Point(self.x+p2.x, self.y+p2.y)
 
     def __mul__(self, p2):
-        return Point(self.x()*p2.x(), self.y()*p2.y())
+        if isinstance(p2, QtCore.QPoint):
+            p2 = Point(p2)      
+        elif not isinstance(p2, Point):
+            raise TypeError("The p2 argument is equal to " \
+                            + str(p2) + " and of type " + str(type(p2)))
+        return Point(self.x*p2.x, self.y*p2.y)
 
     def __div__(self, p2):
-        return Point(self.x()/p2.x(), self.y()/p2.y())
+        if isinstance(p2, QtCore.QPoint):
+            p2 = Point(p2)      
+        elif not isinstance(p2, Point):
+            raise TypeError("The p2 argument is equal to " \
+                            + str(p2) + " and of type " + str(type(p2)))
+        return Point(self.x/p2.x, self.y/p2.y)
 
 
 
 
 class Line(object):
     def __init__(self, p1=Point(), p2=Point()):
-        self.setP1(p1)
-        self.setP2(p2)
+        print "Line"
+        
+        self._p1 = p1
+        self._p2 = p2
     
     #def setLine(self, x1, y1, x2, y2):
-      #  self.__p1 = Point(x1, y1)
-     #   self.__p2 = Point(x2, y2)        
+      #  self._p1 = Point(x1, y1)
+     #   self._p2 = Point(x2, y2)        
 
+    @property
+    def p1(self):
+        return self._p1
 
-    def setP1(self, p1):
-        self.__p1 = p1
+    @p1.setter
+    def p1(self, p1):
+        if isinstance(p1, QtCore.QPoint):
+            self._p1 = Point(p1)
+        elif isinstance(p1, Point):
+            self._p1 = p1
+        else:
+            raise TypeError
         
-    def setP2(self, p2):
-        self.__p2 = p2
         
-    def x1(self) : return self.__p1.x()
-    def y1(self) : return self.__p1.y()
-    def x2(self) : return self.__p2.x()
-    def y2(self) : return self.__p2.y()
+        
+        
+        
+    @property
+    def p2(self):
+        return self._p2
 
-    def p1(self) : return self.__p1
-    def p2(self) : return self.__p2
+    @p2.setter
+    def p2(self, p2):
+        if isinstance(p2, QtCore.QPoint):
+            self._p2 = Point(p2)
+        elif isinstance(p2, Point):
+            self._p2 = p2
+        else:
+            raise TypeError
+        
+        
+        
+        
+    @property
+    def x1(self):
+        if self._p1 is None:
+            return None
+        return self._p1.x      
+        
+    @property
+    def x2(self):
+        if self._p2 is None:
+            return None        
+        return self._p2.x       
+        
+        
+
+    @property
+    def y1(self):
+        if self._p1 is None:
+            return None        
+        return self._p1.y      
+        
+    @property
+    def y2(self):
+        if self._p2 is None:
+            return None        
+        return self._p2.y       
+        
+                
+
 
 
     def length(self):
-        return sqrt((self.p1().x() - self.p2().x())**2 + (self.p1().y() - self.p2().y())**2)
+        return sqrt((self.x1 - self.x2)**2 + (self.y1 - self.y2)**2)
 
     # Based on Antonio, F. "Faster Line Segment Intersection. Ch. IV.6 in 
     # Graphics Gems III (Ed. D. Kirk). San Diego: Academic Press, pp. 199-202 
     # and 500-501, 1992.     
     def isIntersecting(self, l2):
             
-        a = self.p2() - self.p1()
-        b = l2.p1()   - l2.p2()
-        c = self.p1() - l2.p1()
+        a = self.p2 - self.p1
+        b = l2.p1   - l2.p2
+        c = self.p1 - l2.p1
     
-        den = a.y() *b.x() - a.x() * b.y()
+        den = a.y *b.x - a.x * b.y
     
         if den == 0:
             return False
 
-        num1 = b.y()*c.x() - b.x()*c.y()
+        num1 = b.y*c.x - b.x*c.y
         
         if den > 0 :
             if num1 < 0 or num1 > den:
@@ -118,7 +220,7 @@ class Line(object):
         elif num1 > 0 or num1 < den:
             return False
 
-        num2 = a.x()*c.y() - a.y()*c.x()
+        num2 = a.x*c.y - a.y*c.x
 
         if den > 0 :
             if num2 <0 or num2 > den:
