60ef0ed4f826e28d009f8b3fcbc1906ae0ca6878
unknown <oreichri@OREILLYC.crhsc.local>
2015-06-01 14:27:30 -0400

    Update to Python 3.x.

--- spyndle/DevuystDB/__init__.py
+++ spyndle/DevuystDB/__init__.py
@@ -3,4 +3,4 @@ import os
 
 _devuystDBPath_ = os.path.dirname(__file__)
 
-from convertEDFDevuyst import convertDevuystToBDF
+from .convertEDFDevuyst import convertDevuystToBDF
--- spyndle/DevuystDB/convertEDFDevuyst.py
+++ spyndle/DevuystDB/convertEDFDevuyst.py
@@ -12,29 +12,30 @@ import os
 
 def convertDevuystToBDF():
     path = os.path.dirname(__file__)
-    fpath        = u'excerpt'
-    pathV1       = u'Visual_scoring1_excerpt'
-    pathV2       = u'Visual_scoring2_excerpt'
-    pathAu       = u'Automatic_detection_excerpt'
-    pathHy       = u'Hypnogram_excerpt'
+    fpath        = 'excerpt'
+    pathV1       = 'Visual_scoring1_excerpt'
+    pathV2       = 'Visual_scoring2_excerpt'
+    pathAu       = 'Automatic_detection_excerpt'
+    pathHy       = 'Hypnogram_excerpt'
     
     samplingRates = [100.0, 200.0, 50.0, 200.0, 200.0, 200.0, 200.0, 200.0]
 
     
     for i in range(1, 9):
         try:
-            print 'Loading data for subject #' + str(i) + '...'
+            print(('Loading data for subject #' + str(i) + '...'))
             reader = DevuystReader(os.path.join(path, fpath + str(i) + ".txt"), samplingRates[i-1])    
             reader.importEvents(os.path.join(path, pathV1 + str(i) + ".txt"), "spindleV1")
             reader.importEvents(os.path.join(path, pathV2 + str(i) + ".txt"), "spindleV2")
             reader.importEvents(os.path.join(path, pathAu + str(i) + ".txt"), "spindleAu")
             reader.importHypnogram(os.path.join(path, pathHy + str(i) + ".txt"))
     
-            print 'Saving ' + fpath + str(i) + ".BDF" + '...'        
+            print(('Saving ' + fpath + str(i) + ".BDF" + '...'))        
             reader.saveAsEDF(os.path.join(path, fpath + str(i) + ".BDF"), fileType = "BDF")   
                 
-        except IOError:     
-            print "Error: The selected file could not be open."
+        except IOError as e:     
+            print("Error: The selected file could not be open.")
+            print(e)
             exit()        
     
     
