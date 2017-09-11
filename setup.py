6e24b8b6117597a5d4cb26bb83231c8c3e5da55b
Christian O'Reilly <christian.oreilly@epfl.ch>
2017-09-11 11:44:49 +0200

    Adding explicit compatibility with Python 3.3 and 3.5.

--- setup.py
+++ setup.py
@@ -65,7 +65,9 @@ setup(
 			"License :: Free for non-commercial use",
 			"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 			"Natural Language :: English",
+			"Programming Language :: Python :: 3.3",
 			"Programming Language :: Python :: 3.4",
+			"Programming Language :: Python :: 3.5",
 			"Topic :: Scientific/Engineering"])
 	
 	
